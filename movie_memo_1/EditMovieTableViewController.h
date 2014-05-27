32298b3f44f53e0db3101da919bb984de771f8b1
Hironobu Matsuhisa <matsuhisa@gmail.com>
2014-05-28 06:16:34 +0900

    新しい画面を用意

---
+++ movie_memo_1/EditMovieTableViewController.h
@@ -0,0 +1,12 @@
+//
+//  EditMovieTableViewController.h
+//  movie_memo_1
+//
+
+#import <UIKit/UIKit.h>
+
+@class MovieWatch;
+
+@interface EditMovieTableViewController : UITableViewController
+<UITextFieldDelegate>
+@end
